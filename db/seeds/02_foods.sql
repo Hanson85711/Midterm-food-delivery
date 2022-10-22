51fa6a76c0ab0b37d5b425aaecd40e7b196e00d1
stufromnorthvan <stumitchell@irsss.ca>
2022-10-22 15:33:12 -0700

    add to gitignore, seeds for foods

--- db/seeds/02_foods.sql
+++ db/seeds/02_foods.sql
@@ -1,4 +1,4 @@
-INSERT INTO foods (name, category, pic, desc, price) VALUES ();
+INSERT INTO foods (name, category, pic, desc, price) VALUES ('Goth Chocolate', 'drink', 'https://github.com/Hanson85711/Midterm-food-delivery/blob/stu/db/pics/halloween-7476661_1280.png?raw=true', 'A silky, smooth chocolate hot drink with ghost', );
 INSERT INTO foods (name, category, pic, desc, price) VALUES ();
 INSERT INTO foods (name, category, pic, desc, price) VALUES ();
 
