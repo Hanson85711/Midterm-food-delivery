b4f5c1344d2ad3db06f33c5ae1295d42b9a83bb2
Hanson Lee <leehanson@alumni.ubc.ca>
2022-10-23 20:31:01 +0000

    Basic router routes set up

---
+++ routes/foodmenu.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router()
+
+//POST request to delete foods from order
+router.post("/foods/:id/delete", (req, res) => {
+})
+
+//POST request for adding orders to cart
+router.post("/foods", (req, res) => {
+})
+
+//Basic Landing Page which redirects to admin exclusive site if
+router.get('/foods/:id', (req, res) => {
+  if (users[req.session.user_id] === users[restaurantAdminID]) {
+    res.redirect("/foods/admin");
+  }
+  res.render("foods");
+});
+
+//TestPage
+router.get('/', (req, res) => {
+  res.render('testfoods');
+});
+
+module.exports = router;
+
+
+
