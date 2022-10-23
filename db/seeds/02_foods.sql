4cd56d3bb76f8486e8a4288a08d44ca912175f3b
stufromnorthvan <stumitchell@irsss.ca>
2022-10-23 12:36:40 -0700

    make adjustments to db schema and seeds to make functioning db

--- db/seeds/02_foods.sql
+++ db/seeds/02_foods.sql
@@ -1,4 +1,4 @@
-INSERT INTO foods (name, category, pic, desc, price) VALUES ('Goth Chocolate', 'drink', 'https://github.com/Hanson85711/Midterm-food-delivery/blob/stu/db/pics/halloween-7476661_1280.png', 'A silky, smooth hot chocolate with floating ghosts', 3.59);
-INSERT INTO foods (name, category, pic, desc, price) VALUES ('Brain Fog', 'drink', 'https://github.com/Hanson85711/Midterm-food-delivery/blob/seeds/db/pics/halloween-7481539_1280.png', 'A subtle blend of eyeballs, milk and steeped tea', 4.29);
-INSERT INTO foods (name, category, pic, desc, price) VALUES ('Jack Espresso', 'drink', 'https://github.com/Hanson85711/Midterm-food-delivery/blob/seeds/db/pics/halloween-7479703_1280.png', 'A strong, but sophisticated shot of espresso served in a pumpkin', 4.99);;
+INSERT INTO foods (name, category, pic, description, price) VALUES ('Goth Chocolate', 'drink', 'https://github.com/Hanson85711/Midterm-food-delivery/blob/stu/db/pics/halloween-7476661_1280.png', 'A silky, smooth hot chocolate with floating ghosts', 3.59);
+INSERT INTO foods (name, category, pic, description, price) VALUES ('Brain Fog', 'drink', 'https://github.com/Hanson85711/Midterm-food-delivery/blob/seeds/db/pics/halloween-7481539_1280.png', 'A subtle blend of eyeballs, milk and steeped tea', 4.29);
+INSERT INTO foods (name, category, pic, description, price) VALUES ('Jack Espresso', 'drink', 'https://github.com/Hanson85711/Midterm-food-delivery/blob/seeds/db/pics/halloween-7479703_1280.png', 'A strong, but sophisticated shot of espresso served in a pumpkin', 4.99);;
 
