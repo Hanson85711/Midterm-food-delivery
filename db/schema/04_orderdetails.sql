178c65e16e15dd28cdef3e80e7e49c72fa624912
stufromnorthvan <stumitchell@irsss.ca>
2022-10-26 15:32:40 -0700

    add new database for order details

---
+++ db/schema/04_orderdetails.sql
@@ -0,0 +1,10 @@
+-- Drop and recreate Order Details table (Example)
+
+DROP TABLE IF EXISTS orders CASCADE;
+
+CREATE TABLE order_details (
+  id SERIAL PRIMARY KEY NOT NULL,
+  order_id INTEGER REFERENCES orders.id ON DELETE CASCADE,
+  food_id INTEGER REFERENCES foods(id) ON DELETE CASCADE,
+  qty INT DEFAULT 0
+);
