c2d6155ae18643a5c8eb49af843d1120af15f008
stufromnorthvan <stumitchell@irsss.ca>
2022-10-26 17:25:06 -0700

    change queries

--- db/queries/adminorders.js
+++ db/queries/adminorders.js
@@ -2,24 +2,24 @@ const db = require('../connection');
 
 const placeOrder = (userid) => {
   return db.query(`UPDATE orders
-    SET submitted = true
-    WHERE users.id = ${userid}
+    SET submitted = true, order_number =
+    WHERE submitted = false AND users.id = ${userid}
     RETURNING *;`)
     .then(data => {
       return data.rows;
     });
 };
 
-const getSubmittedOrders = () => {
-  return db.query(`SELECT foods.name as food, orders.user_id, count(orders.*), SUM(foods.price) as total_price, foods.id as foodId,
-  foods.pic as pic
-  FROM orders
-  JOIN foods ON foods.id = food_id
-  WHERE submitted = TRUE
-  GROUP BY foods.name, orders.user_id, foods.pic, foods.id;`)
-    .then(data => {
-      return data.rows;
-    });
-};
+// const getSubmittedOrders = () => {
+//   return db.query(`SELECT orders.order_number, foods.name as food, orders.user_id, count(orders.*), SUM(foods.price) as total_price, foods.id as foodId,
+//   foods.pic as pic
+//   FROM orders
+//   JOIN foods ON foods.id = food_id
+//   WHERE submitted = TRUE AND completed = FALSE
+//   GROUP BY orders.order_number, foods.name, orders.user_id, foods.pic, foods.id;`)
+//     .then(data => {
+//       return data.rows;
+//     });
+// };
 
 module.exports = { placeOrder, getSubmittedOrders };
